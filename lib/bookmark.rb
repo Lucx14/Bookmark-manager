340898f69403a918cbb9e2ff0cac1033fbcab2fc
Lucien Najev <luciennajev@hotmail.com>
2018-09-01 09:30:50 +0100

    added connection to the database from the model

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,11 +1,12 @@
+require 'pg'
+
 class Bookmark
 
   def self.all
-    [
-      "http://www.google.com",
-      "http://www.makersacademy.com",
-      "http://www.amazon.com"
-    ]
+
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks')
+    result.map {|bookmark| bookmark['url']}
 
   end
 
